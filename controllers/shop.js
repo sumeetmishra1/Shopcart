352ab269fea4cd8c92616e1b189d7e19aad51a48
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-24 14:16:56 +0530

    sql

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,33 +2,40 @@ const Product = require('../models/product');
 const cart =require('../models/cart');
 const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
-};
+  })
+  .catch(err=>console.log(err));
+}
 exports.getproduct=(req,res,next)=>{
   const proid= req.params.productId;
-  Product.findbyId(proid,product=>{
+  Product.findbyId(proid)
+  .then(([product])=>{
     res.render('shop/product-detail',{
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
     })
   })
+  .catch(err=>console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
-  });
+  })
+  .catch(err=>console.log(err));
+ 
 };
 
 exports.getCart = (req, res, next) => {
