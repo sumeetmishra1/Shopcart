7be377d8f5d97c996d5b133d37d82cba707cdd9e
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-27 15:07:38 +0530

    till associations

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null,title, imageUrl, description, price);
-  product.save().then(()=>{
-    res.redirect('/');
+  req.user.createProduct({
+    title: title,
+    price: price,
+  imageUrl: imageUrl,
+  description:description,
   })
-  .catch(err=>console.log(err));
-  
+  .then((result)=>
+  res.redirect('/')
+  )
+  .catch(err=>{
+    console.log(err);
+  })
+ 
+
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -27,18 +35,20 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const proid=req.params.productId;
-  Product.findbyId(proid,product=>{
+  req.user.getProducts({where:{id:proid}})
+  .then(products=>{
+    const product=products[0];
     if(!product){
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing : editMode,
-      product :product
-    });
-  });
-  
+    return res.redirect('/');
+  }
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    editing : editMode,
+    product :product
+  })
+})
+.catch(err=>console.log(err))
 };
 exports.postEditProduct=(req,res,next)=>{
   const proid=req.body.productId;
@@ -46,22 +56,42 @@ exports.postEditProduct=(req,res,next)=>{
   const UpdatedimageUrl = req.body.imageUrl;
   const Updatedprice = req.body.price;
   const Updateddescription = req.body.description;
-  const Updatedproduct = new Product(proid,Updatedtitle, UpdatedimageUrl, Updateddescription, Updatedprice);
-  Updatedproduct.save();
+  Product.findByPk(proid)
+  .then(product=>{
+    product.title=Updatedtitle;
+    product.price=Updatedprice,
+    product.description=Updateddescription,
+    product.imageUrl=UpdatedimageUrl;
+    return product.save();
+  })
+  .then(result=>{
+    res.redirect('/admin/products');
+  })
+  .catch(err=>console.log(err))
+  
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  req.user.getProducts().then(products=>{
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .then(res=>console.log("done"))
+  .catch(err=>console.log(err));
 };
 
 exports.postdeleteproduct=(req,res,next)=>{
   const proid=req.body.productId;
-  Product.deleteById(proid);
-  res.redirect('/admin/products');
+  Product.findByPk(proid)
+  .then(product=>{
+    return product.destroy();
+  })
+  .then(result=>{
+    res.redirect('/admin/products');
+  })
+  .catch(err=>console.log(err));
+  
 }
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,22 +2,20 @@ const Product = require('../models/product');
 const cart =require('../models/cart');
 const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows]) => {
+  Product.findAll().then(products=>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  })
-  .catch(err=>console.log(err));
+  }).catch(err=>console.log(err));
 }
 exports.getproduct=(req,res,next)=>{
   const proid= req.params.productId;
-  Product.findbyId(proid)
-  .then(([product])=>{
+  Product.findByPk(proid)
+  .then((product)=>{
     res.render('shop/product-detail',{
-      product: product[0],
+      product: product,
       pageTitle: product.title,
       path: '/products'
     })
@@ -26,16 +24,13 @@ exports.getproduct=(req,res,next)=>{
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll().then(products=>{
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
-  })
-  .catch(err=>console.log(err));
- 
+  }).catch(err=>console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
